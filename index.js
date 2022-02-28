dc65e6669304e7d513d40e268a3934e990c6ced5
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-02-28 02:05:17 -0500

    update

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ function calcCharsNoSpaces(str) {
 	return str.replace(/\s/g, '').length
 }
 
+const today = new Date()
+
+
 const insertCalculation = async (calculation) => {
 	const db = connectToFirestore()
 	await db
@@ -39,7 +42,7 @@ const insertCalculation = async (calculation) => {
 				.collection('calculations')
 				.doc(doc.id)
 				.update({
-					result: calcCharsNoSpaces(calculation.string),
+					dateAdded: today.toLocaleString('en-us')
 				})
 		})
 		.catch(console.error)
@@ -47,7 +50,6 @@ const insertCalculation = async (calculation) => {
 
 app.post('/calculations', (request, response) => {
 	const calculation = request.body
-	// const calculation = { string }
 	insertCalculation(calculation)
 		.then(
 			response
@@ -71,16 +73,16 @@ app.get('/calculations/:calcId', (request, response) => {
 		.catch((err) => response.status(500).send(err))
 })
 
-	app.get('/calculations', (request, response) => {
-    const db = connectToFirestore()
-    db.collection('calculations').get()
-    .then(snapshot => {
-        const calculations = snapshot.docs.map(doc => {
-            let calculation = doc.data()
-            calculation.id = doc.id
-            return calculation
-        })
-        response.status(200).send(calculations)
-    })
-    .catch(console.error)
+app.get('/calculations', (request, response) => {
+	const db = connectToFirestore()
+	db.collection('calculations').get()
+		.then((snapshot) => {
+			const calculations = snapshot.docs.map((doc) => {
+				let calculation = doc.data()
+				calculation.id = doc.id
+				return calculation
+			})
+			response.status(200).send(calculations)
+		})
+		.catch((err) => response.status(500).send(err))
 })
